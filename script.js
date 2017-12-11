5a5f18805e82c6223263e915dde3f2cb2058b27f
“Codillac <luk.krolikowski@gmail.com>
2017-12-11 19:23:08 +0100

    Fix URL address

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://frozen-wave-15371.herokuapp.com//v1/task/';
+  const apiRoot = 'https://frozen-wave-15371.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://frozen-wave-15371.herokuapp.com//v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
