a4767c4edee46bb86db5a957e13ad4b23a5d3f84
“Codillac <luk.krolikowski@gmail.com>
2018-01-28 16:54:19 +0100

    Another fixes

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
 
 
     function handleDatatableRender(taskData, boards) {
-        $tasksContainer.empty();
+        tasksContainer.empty();
         boards.forEach(board => {
             availableBoards[board.id] = board;
             });
@@ -56,7 +56,7 @@ $(document).ready(function () {
             .append($availableBoardsOptionElements);
 
         $datatableRowEl
-            .appendTo($tasksContainer);
+            .appendTo(tasksContainer);
         }
     });
 
